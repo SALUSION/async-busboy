7f2d6f12e3f1d6003f6677efafa59565a9d469b7
Moojig Battsogt <me@moojig.dev>
2022-04-14 12:12:45 -0400

    replace busboy dependency with fastify version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Busboy = require('busboy');
+const { default: Busboy } = require('@fastify/busboy');
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
@@ -8,132 +8,134 @@ const path = require('path');
 const getDescriptor = Object.getOwnPropertyDescriptor;
 
 module.exports = function (request, options) {
-  options = options || {};
-  options.headers = options.headers || request.headers;
-  const customOnFile = typeof options.onFile === "function" ? options.onFile : false;
-  delete options.onFile;
-  const busboy = new Busboy(options);
+	options = options || {};
+	options.headers = options.headers || request.headers;
+	const customOnFile =
+		typeof options.onFile === 'function' ? options.onFile : false;
+	delete options.onFile;
+	const busboy = new Busboy(options);
 
-  return new Promise((resolve, reject) => {
-    const fields = {};
-    const filePromises = [];
+	return new Promise((resolve, reject) => {
+		const fields = {};
+		const filePromises = [];
 
-    request.on('close', cleanup);
+		request.on('close', cleanup);
 
-    busboy
-      .on('field', onField.bind(null, fields))
-      .on('file', customOnFile || onFile.bind(null, filePromises))
-      .on('error', onError)
-      .on('end', onEnd)
-      .on('finish', onEnd);
+		busboy
+			.on('field', onField.bind(null, fields))
+			.on('file', customOnFile || onFile.bind(null, filePromises))
+			.on('error', onError)
+			.on('end', onEnd)
+			.on('finish', onEnd);
 
-    busboy.on('partsLimit', function(){
-      const err = new Error('Reach parts limit');
-      err.code = 'Request_parts_limit';
-      err.status = 413;
-      onError(err);
-    });
+		busboy.on('partsLimit', function () {
+			const err = new Error('Reach parts limit');
+			err.code = 'Request_parts_limit';
+			err.status = 413;
+			onError(err);
+		});
 
-    busboy.on('filesLimit', () => {
-      const err = new Error('Reach files limit');
-      err.code = 'Request_files_limit';
-      err.status = 413;
-      onError(err);
-    });
+		busboy.on('filesLimit', () => {
+			const err = new Error('Reach files limit');
+			err.code = 'Request_files_limit';
+			err.status = 413;
+			onError(err);
+		});
 
-    busboy.on('fieldsLimit', () => {
-      const err = new Error('Reach fields limit');
-      err.code = 'Request_fields_limit';
-      err.status = 413;
-      onError(err);
-    });
+		busboy.on('fieldsLimit', () => {
+			const err = new Error('Reach fields limit');
+			err.code = 'Request_fields_limit';
+			err.status = 413;
+			onError(err);
+		});
 
-    request.pipe(busboy);
+		request.pipe(busboy);
 
-    function onError(err) {
-      cleanup();
-      return reject(err);
-    }
+		function onError(err) {
+			cleanup();
+			return reject(err);
+		}
 
-    function onEnd(err) {
-      if(err) {
-        return reject(err);
-      }
-      if (customOnFile) {
-        cleanup();
-        resolve({ fields });
-      } else {
-        Promise.all(filePromises)
-          .then((files) => {
-            cleanup();
-            resolve({fields, files});
-          })
-          .catch(reject);
-      }
-    }
+		function onEnd(err) {
+			if (err) {
+				return reject(err);
+			}
+			if (customOnFile) {
+				cleanup();
+				resolve({ fields });
+			} else {
+				Promise.all(filePromises)
+					.then((files) => {
+						cleanup();
+						resolve({ fields, files });
+					})
+					.catch(reject);
+			}
+		}
 
-    function cleanup() {
-      busboy.removeListener('field', onField);
-      busboy.removeListener('file', customOnFile || onFile);
-      busboy.removeListener('close', cleanup);
-      busboy.removeListener('end', cleanup);
-      busboy.removeListener('error', onEnd);
-      busboy.removeListener('partsLimit', onEnd);
-      busboy.removeListener('filesLimit', onEnd);
-      busboy.removeListener('fieldsLimit', onEnd);
-      busboy.removeListener('finish', onEnd);
-    }
-  });
+		function cleanup() {
+			busboy.removeListener('field', onField);
+			busboy.removeListener('file', customOnFile || onFile);
+			busboy.removeListener('close', cleanup);
+			busboy.removeListener('end', cleanup);
+			busboy.removeListener('error', onEnd);
+			busboy.removeListener('partsLimit', onEnd);
+			busboy.removeListener('filesLimit', onEnd);
+			busboy.removeListener('fieldsLimit', onEnd);
+			busboy.removeListener('finish', onEnd);
+		}
+	});
 };
 
 function onField(fields, name, val, fieldnameTruncated, valTruncated) {
-  // don't overwrite prototypes
-  if (getDescriptor(Object.prototype, name)) return;
+	// don't overwrite prototypes
+	if (getDescriptor(Object.prototype, name)) return;
 
-  // This looks like a stringified array, let's parse it
-  if (name.indexOf('[') > -1) {
-    const obj = objectFromBluePrint(extractFormData(name), val);
-    reconcile(obj, fields);
-
-  } else {
-    if (fields.hasOwnProperty(name)) {
-      if (Array.isArray(fields[name])) {
-        fields[name].push(val);
-      } else {
-        fields[name] = [fields[name], val];
-      }
-    } else {
-      fields[name] = val;
-    }
-  }
+	// This looks like a stringified array, let's parse it
+	if (name.indexOf('[') > -1) {
+		const obj = objectFromBluePrint(extractFormData(name), val);
+		reconcile(obj, fields);
+	} else {
+		if (fields.hasOwnProperty(name)) {
+			if (Array.isArray(fields[name])) {
+				fields[name].push(val);
+			} else {
+				fields[name] = [fields[name], val];
+			}
+		} else {
+			fields[name] = val;
+		}
+	}
 }
 
 function onFile(filePromises, fieldname, file, filename, encoding, mimetype) {
-  const tmpName = file.tmpName = Math.random().toString(16).substring(2) + '-' + filename;
-  const saveTo = path.join(os.tmpdir(), path.basename(tmpName));
-  const writeStream = fs.createWriteStream(saveTo);
+	const tmpName = (file.tmpName =
+		Math.random().toString(16).substring(2) + '-' + filename);
+	const saveTo = path.join(os.tmpdir(), path.basename(tmpName));
+	const writeStream = fs.createWriteStream(saveTo);
 
-  const filePromise = new Promise((resolve, reject) => writeStream
-    .on('open', () => file
-      .pipe(writeStream)
-      .on('error', reject)
-      .on('finish', () => {
-        const readStream = fs.createReadStream(saveTo);
-        readStream.fieldname = fieldname;
-        readStream.filename = filename;
-        readStream.transferEncoding = readStream.encoding = encoding;
-        readStream.mimeType = readStream.mime = mimetype;
-        resolve(readStream);
-      })
-    )
-    .on('error', (err) => {
-      file
-        .resume()
-        .on('error', reject);
-      reject(err);
-    })
-  );
-  filePromises.push(filePromise);
+	const filePromise = new Promise((resolve, reject) =>
+		writeStream
+			.on('open', () =>
+				file
+					.pipe(writeStream)
+					.on('error', reject)
+					.on('finish', () => {
+						const readStream = fs.createReadStream(saveTo);
+						readStream.fieldname = fieldname;
+						readStream.filename = filename;
+						readStream.transferEncoding = readStream.encoding =
+							encoding;
+						readStream.mimeType = readStream.mime = mimetype;
+						resolve(readStream);
+					})
+			)
+			.on('error', (err) => {
+				file.resume().on('error', reject);
+				reject(err);
+			})
+	);
+	filePromises.push(filePromise);
 }
 
 /**
@@ -148,11 +150,11 @@ function onFile(filePromises, fieldname, file, filename, encoding, mimetype) {
  *
  */
 const extractFormData = (string) => {
-  const arr = string.split('[');
-  const first = arr.shift();
-  const res = arr.map( v => v.split(']')[0] );
-  res.unshift(first);
-  return res;
+	const arr = string.split('[');
+	const first = arr.shift();
+	const res = arr.map((v) => v.split(']')[0]);
+	res.unshift(first);
+	return res;
 };
 
 /**
@@ -167,17 +169,15 @@ const extractFormData = (string) => {
  *
  */
 const objectFromBluePrint = (arr, value) => {
-  return arr
-    .reverse()
-    .reduce((acc, next) => {
-      if (Number(next).toString() === 'NaN') {
-        return {[next]: acc};
-      } else {
-        const newAcc = [];
-        newAcc[ Number(next) ] = acc;
-        return newAcc;
-      }
-    }, value);
+	return arr.reverse().reduce((acc, next) => {
+		if (Number(next).toString() === 'NaN') {
+			return { [next]: acc };
+		} else {
+			const newAcc = [];
+			newAcc[Number(next)] = acc;
+			return newAcc;
+		}
+	}, value);
 };
 
 /**
@@ -189,18 +189,17 @@ const objectFromBluePrint = (arr, value) => {
  *
  */
 const reconcile = (obj, target) => {
-  const key = Object.keys(obj)[0];
-  const val = obj[key];
+	const key = Object.keys(obj)[0];
+	const val = obj[key];
 
-  // The reconciliation works even with array has
-  // Object.keys will yield the array indexes
-  // see https://jsbin.com/hulekomopo/1/
-  // Since array are in form of [ , , valu3] [value1, value2]
-  // the final array will be: [value1, value2, value3] has expected
-  if (target.hasOwnProperty(key)) {
-    return reconcile(val, target[key]);
-  } else {
-    return target[key] = val;
-  }
-
+	// The reconciliation works even with array has
+	// Object.keys will yield the array indexes
+	// see https://jsbin.com/hulekomopo/1/
+	// Since array are in form of [ , , valu3] [value1, value2]
+	// the final array will be: [value1, value2, value3] has expected
+	if (target.hasOwnProperty(key)) {
+		return reconcile(val, target[key]);
+	} else {
+		return (target[key] = val);
+	}
 };
