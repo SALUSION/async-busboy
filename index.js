182dd4cd2f71aeb958348f1867becdc129cebf14
Moojig Battsogt <me@moojig.dev>
2022-04-14 12:20:21 -0400

    remove .removeListener('close')

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ module.exports = function (request, options) {
 		function cleanup() {
 			busboy.removeListener('field', onField);
 			busboy.removeListener('file', customOnFile || onFile);
-			busboy.removeListener('close', cleanup);
+			// busboy.removeListener('close', cleanup);
 			busboy.removeListener('end', cleanup);
 			busboy.removeListener('error', onEnd);
 			busboy.removeListener('partsLimit', onEnd);
